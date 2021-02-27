f9de101f464ea02f61627d1ed8d196cd39e9ff74
sadmanishrak <50618180+sadmanishrak@users.noreply.github.com>
2021-02-27 15:18:39 +0600

    Add files via upload

---
+++ coding_challenge_5.js
@@ -0,0 +1,12 @@
+const firstScoreKoala = Number(prompt('Enter 1st Koala score'));
+const secondScoreKoala = Number(prompt('Enter 2nd Koala score'));
+const thirdScoreKoala = Number(prompt('Enter 3rd Koala score'));
+
+const firstScoreDolphins = Number(prompt('Enter 1st Dolphins score'));
+const secondScoreDolphins = Number(prompt('Enter 2nd Dolphins score'));
+const thirdScoreDolphins = Number(prompt('Enter 3rd Dolphins score'));
+
+const avgDolphins = calcAvg(firstScoreDolphins, secondScoreDolphins, thirdScoreDolphins);
+const avgKoalas = calcAvg(firstScoreKoala, secondScoreKoala, thirdScoreKoala);
+
+checkWinner(avgDolphins, avgKoalas);
