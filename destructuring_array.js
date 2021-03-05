d9f6dc075df300aaedcb3862982ecf579d1f714e
sadmanishrak <50618180+sadmanishrak@users.noreply.github.com>
2021-03-05 13:19:51 +0600

    Add files via upload

---
+++ destructuring_array.js
@@ -0,0 +1,37 @@
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(restaurant.order(2, 0));
+
+const [starterCourse, mainCourse] = restaurant.order(2, 0);
+console.log(starterCourse, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// let [i, , j] = nested;
+// console.log(i, j);
+
+let [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
