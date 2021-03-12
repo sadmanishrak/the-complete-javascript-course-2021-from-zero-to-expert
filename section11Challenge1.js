8844b743ddd1d2de040f4d8a8a51375bc6a896f5
sadmanishrak <50618180+sadmanishrak@users.noreply.github.com>
2021-03-12 13:10:18 +0600

    Create section11Challenge1.js

---
+++ section11Challenge1.js
@@ -0,0 +1,13 @@
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an dult and is ${dog} years old.`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy.`);
+    }
+  });
+};
