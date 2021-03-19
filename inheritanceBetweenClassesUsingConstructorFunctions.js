2cf17b8137beebfffed288e4f2f9c70f59c0b40b
sadmanishrak <50618180+sadmanishrak@users.noreply.github.com>
2021-03-19 19:41:59 +0600

    Create inheritanceBetweenClassesUsingConstructorFunctions.js

---
+++ inheritanceBetweenClassesUsingConstructorFunctions.js
@@ -0,0 +1,28 @@
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student("Mike", 2021, "Computer Science");
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
